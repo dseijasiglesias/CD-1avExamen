4a4cbc0c9fdc9df80ee2885923459d2e98df8723
dseijasiglesias <dseijasiglesias@eniac08.danielcastelao.org>
2014-12-04 10:15:38 +0100

    revert commit damian

--- src/examencod/ExamenCOD.java
+++ src/examencod/ExamenCOD.java
@@ -7,40 +7,26 @@ public class ExamenCOD {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        int introduceNumero = 11;
-        
-        numeroPrimo(introduceNumero);
-
-        inprimirNumerosPrimos();
-    }
-
-    private static void inprimirNumerosPrimos() {
-        for (int x = 2; x < 1000; x++) {
-            if (metodoPrimo(x)) {
-                System.out.print(x + " ");
-            }
-        }
-    }
-
-    private static void numeroPrimo(int iN) {
-        if (metodoPrimo(iN)) {
-            System.out.println("Es numero primo");
-        } else {
-            System.out.println("NO es numero primo");
-        }
-    }
-
-    public static boolean metodoPrimo(int dato) {
-        int j = 2;
-        boolean aux = true;
-        while ((aux) && (j != dato)) {
-            if (dato % j == 0) {
-                aux = false;
-            }
-            j++;
-        }
-        return aux;
-    }
-
+ int iN = 11;
+            	if (metodoMio(iN))
+System.out.println("Es numero primo");
+else
+ System.out.println("NO es numero primo");           	 
+          	 
+for (int x=2;x<1000;x++) {
+if (metodoMio(x))
+System.out.print(x + " ");                         	 
+}         	 
+	}
+public static boolean metodoMio(int variable){
+int j = 2;
+boolean aux=true;
+while ((aux) && (j!=variable)){
+if (variable % j == 0)
+aux = false;
+j++;
+}
+return aux;
+  } 	 
+    
 }
